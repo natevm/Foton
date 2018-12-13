407af45dad7e0adbc40a3fe231ac488558f38367
Nathan Morrical <natemorrical@gmail.com>
2018-12-12 21:20:33 -0700

    Updated PythonSystem. Had a bug with multithreading

--- Pluto/Systems/PythonSystem/PythonSystem.cxx
+++ Pluto/Systems/PythonSystem/PythonSystem.cxx
@@ -19,12 +19,8 @@
 
 namespace Systems {
     PythonSystem::PythonSystem() {
-        wchar_t *name = L"Pluto";
-        Py_SetProgramName(name);
-        Py_Initialize();
     }
     PythonSystem::~PythonSystem() {
-        Py_Finalize();
     }
 
     PythonSystem* PythonSystem::Get() {
@@ -53,54 +49,42 @@ namespace Systems {
         auto loop = [this]() {
             std::cout<<"Starting PythonSystem, thread id: " << std::this_thread::get_id()<<std::endl;
 
+            wchar_t *name = L"Pluto";
+            Py_SetProgramName(name);
+            Py_Initialize();
+
             Py_InspectFlag = 1;
             Py_InteractiveFlag = 1;
 
             /* Construct wchar_t array */
             using namespace std;
 
-        //     std::string sArgs = std::string("Pluto.exe ") + Options::GetPythonArgs();
-        //    // istringstream iss(sArgs);
-
-        //     std::istringstream iss(sArgs);
-        //     std::vector<std::string> tokens;
-        //     std::string s;
-
-        //     while (iss >> std::quoted(s)) {
-        //         tokens.push_back(s);
-        //     }
-
-        //     for(auto& str: tokens)
-        //         std::cout << str << std::endl;
-
-            // vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
-            // int argc = (int)tokens.size();
-            // std::vector<wchar_t*> new_argv(argc);
-            // for (int i = 0; i < argc; ++i) {
-            //     new_argv[i] = GetWC(tokens[i].c_str());
-            // }
-            // auto wargv = new_argv.data();
-            
-            // PySys_SetArgv(argc, wargv);
-
             if (Options::IsIPyKernel())
             {
-                std::vector<wchar_t*> argv(2);
-                argv[0] = L"Pluto";
-                argv[1] = L"-f";
-                std::string connectionFile = Options::GetConnectionFile();
-                argv[2] = GetWC(connectionFile.c_str());
-                PySys_SetArgv(3, argv.data());
-                PyRun_SimpleString("import sys");
-                PyRun_SimpleString("if sys.path[0] == '': del sys.path[0]\n\n");
-                PyRun_SimpleString("from ipykernel import kernelapp as app");
-                PyRun_SimpleString("app.launch_new_instance()");
+                 #ifdef NDEBUG
+                // nondebug
+                    std::vector<wchar_t*> argv(3);
+                    argv[0] = L"Pluto";
+                    argv[1] = L"-f";
+                    std::string connectionFile = Options::GetConnectionFile();
+                    argv[2] = GetWC(connectionFile.c_str());
+                    PySys_SetArgv(3, argv.data());
+                    PyRun_SimpleString("import sys");
+                    PyRun_SimpleString("if sys.path[0] == '': del sys.path[0]\n\n");
+                    PyRun_SimpleString("from ipykernel import kernelapp as app");
+                    PyRun_SimpleString("app.launch_new_instance()");
+                #else
+                // debug code
+                    std::cout<<"Pluto Error: ipykernel mode does not work when Pluto is built using debug mode. This is due to a bug with pyzmq. See the following issue for details: https://github.com/zeromq/pyzmq/issues/1245 "<<std::endl;
+                #endif
+
             }
             else {
               PyRun_InteractiveLoop(stdin, "<stdin>");
             }
 
-            wchar_t * name = (wchar_t*)"PythonSystem";
+            Py_Finalize();
+            
 
             Systems::EventSystem::Get()->stop();
         };
