9c40564ab9b5e82cbbb1e47b401170b24a4264d7
Nathan Morrical <natemorrical@gmail.com>
2019-04-12 10:25:14 -0600

    More shadowmap fixes...

--- Pluto/Light/Light.cxx
+++ Pluto/Light/Light.cxx
@@ -225,15 +225,15 @@ void Light::CreateShadowCameras()
     // Right, Left, Up, Down, Far, Near
     /* Create shadow map textures */
     for (uint32_t i = 0; i < MAX_LIGHTS; ++i) {
-        auto cam = Camera::Create("ShadowCam_" + std::to_string(i), true, 1024, 1024, 1, 6, false, false);
+        auto cam = Camera::Create("ShadowCam_" + std::to_string(i), true, 512, 512, 1, 6, false, false);
         cam->set_perspective_projection(3.14f * .5f, 1.f, 1.f, .1f, 0);
         cam->set_perspective_projection(3.14f * .5f, 1.f, 1.f, .1f, 1);
         cam->set_perspective_projection(3.14f * .5f, 1.f, 1.f, .1f, 2);
         cam->set_perspective_projection(3.14f * .5f, 1.f, 1.f, .1f, 3);
         cam->set_perspective_projection(3.14f * .5f, 1.f, 1.f, .1f, 4);
         cam->set_perspective_projection(3.14f * .5f, 1.f, 1.f, .1f, 5);
-        cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3( 1,  0,  0), glm::vec3(0.0, 0.0, 1.0)), 0);
-        cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3(-1,  0,  0), glm::vec3(0.0, 0.0, 1.0)), 1);
+        cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3(-1,  0,  0), glm::vec3(0.0, 0.0, 1.0)), 0);
+        cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3( 1,  0,  0), glm::vec3(0.0, 0.0, 1.0)), 1);
         cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3( 0,  0,  1), glm::vec3(0.0, 1.0, 0.0)), 2);
         cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3( 0, 0,  -1), glm::vec3(0.0, -1.0, 0.0)), 3);
         cam->set_view(glm::lookAt(glm::vec3(0.0, 0, 0), glm::vec3( 0,  -1,  0), glm::vec3(0.0, 0.0, 1.0)), 4);
